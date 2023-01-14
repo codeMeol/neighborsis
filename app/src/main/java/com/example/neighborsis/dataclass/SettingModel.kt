528c28211b38d923ad32e9c9edde90dc98a23ee7
codeMeol <hjy7485@gmail.com>
2023-01-14 15:02:11 +0900

    [setting페이지 작업]
    1. 세팅아이템 생성
    2. 세팅 어댑터 생성
    3. 세팅 모델 생성

---
+++ app/src/main/java/com/example/neighborsis/dataclass/SettingModel.kt
@@ -0,0 +1,12 @@
+package com.example.neighborsis.dataclass
+
+import android.widget.ImageView
+import android.widget.TextView
+
+data class SettingModel(
+    val settingExplanationIcon:ImageView,
+    val settingExplanationText:TextView,
+    val settingValueText:TextView,
+    val settingValueRightIcon:ImageView
+
+)
