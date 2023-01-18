eebae3439427eca5121737439b9911383cd11cd8
telejunyoung.heo <telejuntoung.heo@lgepartner.com>
2023-01-18 17:31:39 +0900

    [neighborsis][2023-01-16][cafe24 api 인증 구현]
    [NA]
    [MODEL] samsung SM-A505N
    [DESC]
    
    cafe24 api 인증 구현 retrofit2

---
+++ app/src/main/java/com/example/neighborsis/retrofit2/RetrofitInterface/NotificationAPI.kt
@@ -0,0 +1,31 @@
+package com.example.neighborsis.retrofit2.RetrofitInterface
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.example.neighborsis.dataclass.PushNotification
+import com.example.neighborsis.retrofit2.Constants.CafeConstants.Companion.BASE_URL
+import com.example.neighborsis.retrofit2.Constants.CafeConstants.Companion.CLIENT_ID
+import com.example.neighborsis.retrofit2.Constants.CafeConstants.Companion.CODE_CHALLENGE
+import com.example.neighborsis.retrofit2.Constants.CafeConstants.Companion.REDIRECT_URL
+import com.example.neighborsis.retrofit2.Constants.PushConstants.Companion.CONTENT_TYPE
+import com.example.neighborsis.retrofit2.Constants.PushConstants.Companion.SERVER_KEY
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+interface NotificationAPI {
+
+    @Headers("Authorization: key=$SERVER_KEY", "Content-Type:$CONTENT_TYPE")
+    @POST("fcm/send")
+    suspend fun postNotification(
+        @Body notification: PushNotification
+    ): Response<ResponseBody>
+
+    //https://{mall_id}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id={client_id}&state={encode_csrf_token}
+    //&redirect_uri={encode_redirect_uri}&scope={scope}
+
+
+}
---
+++ app/src/main/java/com/example/neighborsis/util/Pkce.kt
@@ -0,0 +1,31 @@
+package com.example.neighborsis.util
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.io.UnsupportedEncodingException
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.security.SecureRandom
+import java.util.*
+
+
+class Pkce {
+    @RequiresApi(Build.VERSION_CODES.O)
+    @Throws(UnsupportedEncodingException::class)
+    fun generateCodeVerifier(): String? {
+        val secureRandom = SecureRandom()
+        val codeVerifier = ByteArray(32)
+        secureRandom.nextBytes(codeVerifier)
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(codeVerifier)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @Throws(UnsupportedEncodingException::class, NoSuchAlgorithmException::class)
+    fun generateCodeChallange(codeVerifier: String): String? {
+        val bytes = codeVerifier.toByteArray(charset("US-ASCII"))
+        val messageDigest: MessageDigest = MessageDigest.getInstance("SHA-256")
+        messageDigest.update(bytes, 0, bytes.size)
+        val digest: ByteArray = messageDigest.digest()
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(digest)
+    }
+}
